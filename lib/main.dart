40aaeae84108a938e07c1e25b99ca4435bfdb555
Shahpasand <ra.shahpasand@gmail.com>
2021-04-17 22:56:18 +0430

    add bottomNavBar and screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media_profile_screen/screens/home_screen.dart';
+import 'package:social_media_profile_screen/screens/movie_screen.dart';
+import 'package:social_media_profile_screen/screens/profile_screen.dart';
+import 'package:social_media_profile_screen/screens/search_screen.dart';
+import 'package:social_media_profile_screen/screens/shopping_screen.dart';
 
 const iconsPath = 'assets/icons/';
 const imagePath = 'assets/images/';
@@ -11,14 +17,73 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Social Media Profile Screen',
-      home: ProfileScreen(),
+      home: Dashboard(),
     );
   }
 }
 
-class ProfileScreen extends StatelessWidget {
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  final List<Widget> _children = [
+    HomeScreen(),
+    SearchScreen(),
+    MovieScreen(),
+    ShoppingScreen(),
+    ProfileScreen(),
+  ];
+  final iconColor = Colors.black;
+  int _currentTabIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: _children[_currentTabIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentTabIndex,
+        onTap: (index) => setState(() => _currentTabIndex = index),
+        items: [
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(
+              Icons.home,
+              color: iconColor,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(
+              CupertinoIcons.search,
+              color: iconColor,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(
+              Icons.movie_creation_outlined,
+              color: iconColor,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(
+              CupertinoIcons.bag,
+              color: iconColor,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: '',
+            icon: CircleAvatar(
+              child: Image.asset(
+                '${imagePath}avatar.png',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
